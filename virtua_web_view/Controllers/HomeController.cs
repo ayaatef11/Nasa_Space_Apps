8ebe8a3ff75eefbf924400ee2e25ac051f730f3b
aya atef <ayaatef21ayaatef@outlook.com>
2024-10-04 17:38:25 +0300

    make the virtual view live

---
+++ virtua_web_view/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using virtua_web_view.Models;
+
+namespace virtua_web_view.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return File("/orbit.html", "text/html");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ virtua_web_view/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace virtua_web_view.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
